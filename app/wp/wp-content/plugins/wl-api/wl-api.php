269de93d9f067e3f461556dd8769bcf104113efd
bura1 <burictomislavog@gmail.com>
2021-09-23 22:11:13 +0200

    custom REST route to display news

--- app/wp/wp-content/plugins/wl-api/wl-api.php
+++ app/wp/wp-content/plugins/wl-api/wl-api.php
@@ -4,13 +4,18 @@
  * Plugin Name: wl-api
  */
 
-function wl_articles() {
-	return 'return';
+function articles() {
+
+    $response = wp_remote_get('https://inshortsapi.vercel.app/news?category=science');
+    $body = wp_remote_retrieve_body($response);
+    $data = json_decode($body, true);
+
+	return $data;
 }
 
 add_action('rest_api_init', function() {
 	register_rest_route( 'wl/v1', 'posts', [
 		'methods' => 'GET',
-		'callback' => 'wl_articles'
+		'callback' => 'articles'
 	]);
 });
