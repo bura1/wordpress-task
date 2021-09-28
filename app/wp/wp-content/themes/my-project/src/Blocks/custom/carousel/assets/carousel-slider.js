ecf1f125555744c1f2000ede76d9e95e2c758eb9
bura1 <burictomislavog@gmail.com>
2021-09-29 01:42:55 +0200

    added carousel block and customized to fit design

---
+++ app/wp/wp-content/themes/my-project/src/Blocks/custom/carousel/assets/carousel-slider.js
@@ -0,0 +1,59 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+import globalManifest from './../../../manifest.json';
+
+export class CarouselSlider {
+	constructor(options) {
+		Swiper.use([Navigation, Pagination]);
+
+		this.element = options.element;
+		this.blockClass = options.blockClass;
+		this.nextElement = options.nextElement;
+		this.prevElement = options.prevElement;
+		this.paginationElement = options.paginationElement;
+		this.eventName = options.eventName;
+	}
+
+	init() {
+		const { breakpoints } = globalManifest.globalVariables;
+		
+		const item = this.element;
+
+		const showItems = item?.dataset?.showItems ?? -1;
+		const loopMode = item?.dataset?.swiperLoop ?? 'false';
+		
+
+		new Swiper(item, {
+			loop: loopMode === 'true',
+			slideClass: `${this.blockClass}__item`,
+			slidesPerView: 'auto',
+			centeredSlides: true,
+			spaceBetween: 30,
+			keyboard: {
+				enabled: true,
+			},
+			grabCursor: true,
+			breakpointsInverse: true,
+			threshold: 20,
+			navigation: {
+				nextEl: this.nextElement,
+				prevEl: this.prevElement,
+			},
+			pagination: {
+				el: this.paginationElement,
+				type: 'fraction',
+				clickable: true,
+			},
+			breakpoints: {
+				[breakpoints.tablet]: {
+					slidesPerView: parseInt(showItems) === -1 ? 'auto' : parseInt(showItems),
+					spaceBetween: 10,
+				},
+			},
+			on: {
+				init: () => {
+					window.dispatchEvent(this.eventName);
+				},
+			},
+		});
+	}
+}
